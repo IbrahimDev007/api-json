171ffe9110bed559df20a8eb1e9433e44fc88d81
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-22 23:40:16 +0600

    apim-json convert

---
+++ jason.js
@@ -0,0 +1,10 @@
+const user={
+    name:'fahim',
+    roll:2334,
+    collage:'fokir hat collge'
+}
+
+console.log(user);
+console.log(JSON.stringify(user));
+const userst=JSON.stringify(user);
+console.log(JSON.parse(userst));
